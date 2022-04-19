59bb05fc889c777a9b33af92f1f25056a24f70d4
Michael Thai <huy.thai@ymail.com>
2022-04-19 23:36:33 +0700

    add packages

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    webpack: (config, options) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@mui/base': '@mui/base/legacy',
+            '@mui/lab': '@mui/lab/legacy',
+            '@mui/material': '@mui/material/legacy',
+            '@mui/styled-engine': '@mui/styled-engine-sc',
+            '@mui/system': '@mui/system/legacy',
+        };
+        config.module.rules.push({});
+        console.log('test webpack');
+        console.log(config.resolve);
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
