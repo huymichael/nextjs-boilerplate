7d2a67a1ad1defcc707e02a08a0de252d0bd0146
Michael Thai <huy.thai@ymail.com>
2022-05-01 16:57:13 +0700

    config webpack

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'public/styles')],
+    },
     webpack: (config, options) => {
+        // config.output = {
+        //     ...config.output,
+        //     clean: true,
+        // };
+        console.log([path.resolve(__dirname, 'public')]);
         config.resolve.alias = {
             ...config.resolve.alias,
             '@mui/base': '@mui/base/legacy',
@@ -10,9 +20,29 @@ const nextConfig = {
             '@mui/styled-engine': '@mui/styled-engine-sc',
             '@mui/system': '@mui/system/legacy',
         };
-        config.module.rules.push({});
-        console.log('test webpack');
-        console.log(config.resolve);
+        config.module.rules.push(
+            {
+                test: /\.(js|jsx|ts|tsx|)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2|png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            publicPath: [path.resolve(__dirname, 'public')],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack', 'url-loader'],
+            },
+        );
         return config;
     },
 };
