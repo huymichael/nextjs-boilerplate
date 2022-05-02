fe69fdcde3b02d9a87c31fe71abf8245f5948913
Michael Thai <huy.thai@ymail.com>
2022-05-02 16:36:26 +0700

    update view

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,9 +1,16 @@
+import Head from 'next/head';
 import React from 'react';
 import '../public/styles/global.styles.scss';
 
 function MyApp({Component, pageProps}) {
     return (
         <React.Fragment>
+            <Head>
+                <title>
+                    <meta name="viewport"
+                          content="initial-scale=1.0, width=device-width"/>
+                </title>
+            </Head>
             <Component {...pageProps} />
         </React.Fragment>
     );
---
+++ front-end/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+            <div>
+                Test page
+            </div>
+        </>
+    );
+};
+export default HomePage;
